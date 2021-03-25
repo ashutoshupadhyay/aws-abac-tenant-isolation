33db8cdaf28edfbccb2370dbb5de159c3a5b1cef
ashutoshupadhyay <toashutoshupadhyay@gmail.com>
2021-03-25 09:40:35 +0800

    Initial upload
    Lambda function

---
+++ sts-ti-demo-s3-lambda.py
@@ -0,0 +1,77 @@
+import json
+import os
+
+def lambda_handler(event, context):
+    import boto3
+    bucket_name     =   os.environ['s3_bucket_name']
+
+    try:
+        login_tenant_id =   event['login_tenant_id']
+        data_tenant_id  =   event['s3_tenant_home']
+    except:
+        return {
+            'statusCode': 400,
+            'statusDesc': 'Lambda failed',
+            'assumeRole': '',
+            'eventDump': event,
+            'bodyText': 'Error in reading parameters'
+        }
+
+    prefix_of_role  =   'assumeRole'
+    file_name       =   'tenant.info' + '-' + data_tenant_id
+
+    #### NEW CODE
+    # create an STS client object that represents a live connection to the 
+    # STS service
+    sts_client = boto3.client('sts')
+    account_of_role = sts_client.get_caller_identity()['Account']
+    role_to_assume  =   'arn:aws:iam::' + account_of_role + ':role/' + prefix_of_role + '-' + login_tenant_id
+
+    # Call the assume_role method of the STSConnection object and pass the role
+    # ARN and a role session name.
+    RoleSessionName = now.strftime("%d/%m/%Y %H:%M:%S")
+    print("date and time =", dt_string)	
+    try:
+        print('role_to_assume =', role_to_assume)
+        assumed_role_object = sts_client.assume_role(
+            RoleArn=role_to_assume, 
+            RoleSessionName="AssumeRoleSession1", 
+            DurationSeconds = 900)
+    except:
+        return {
+            'statusCode': 400,
+            'statusDesc': 'Lambda failed',
+            'assumeRole': role_to_assume,
+            'eventDump': event,
+            'bodyText': 'Error in assuming the role ' + role_to_assume + ' in account ' + account_of_role
+        }
+
+    # From the response that contains the assumed role, get the temporary 
+    # credentials that can be used to make subsequent API calls
+    credentials=assumed_role_object['Credentials']
+    
+    # Use the temporary credentials that AssumeRole returns to make a 
+    # connection to Amazon S3  
+    s3_resource=boto3.resource(
+        's3',
+        aws_access_key_id=credentials['AccessKeyId'],
+        aws_secret_access_key=credentials['SecretAccessKey'],
+        aws_session_token=credentials['SessionToken']
+    )
+    ####
+
+    try:
+        obj = s3_resource.Object(bucket_name, data_tenant_id + "/" + file_name)
+        body = obj.get()['Body'].read()
+    except:
+        body = 'error in reading s3://' + bucket_name + '/' + data_tenant_id + '/' + file_name
+
+    print (body)
+
+    return {
+        'statusCode': 200,
+        'statusDesc': 'Lambda success',
+        'assumeRole': role_to_assume,
+        'eventDump': event,
+        'bodyText': body
+    }
